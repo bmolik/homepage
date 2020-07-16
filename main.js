8bf10f35ccb5b5d3d961bdbb080659e132574030
Bartosz Molik <brth182@gmail.com>
2020-07-17 01:35:59 +0200

    changed layout, used BEM rules and added JS func

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const name = 'Kotek';
+const name = "Kotek";
 const age = 1.5;
-
 
 console.log(name);
 console.log(age);
-console.log(`Kotek ${name} tak. I ma ${age} latek.`)
+console.log(`Kotek ${name} tak. I ma ${age} latek.`);
+
+const message = document.querySelector('.hidden-one__header');
+message.innerHTML = "Łajby są super!!!";
